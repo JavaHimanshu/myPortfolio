861ea24a47f596bd0735b8ec9db6545dbacb3dce
JavaHimanshu <140224061+JavaHimanshu@users.noreply.github.com>
2023-09-01 11:39:59 +0530

    Delete js directory

--- js/autoTypingAnimation.js
+++
@@ -1,9 +0,0 @@
-setTimeout(function() {
-    $('.about-info-inner').typed({
-      strings: [
-        "<span>Hi, I am Himanshu Chaurasiya</span> Completed Graduation in BSc.IT with skills like Java , Python , MYSQL , C# , MongoDB , C++ and Data Structure. An adaptable and responsible Student seeking an entry into IT Sector. An excellent academic record with active participation in activities shows myself to be selfmotivated, organized and capable of working under pressure. Science studies gave me a logical mind and problem solving attitude. Eagerly looking forward to learn and grow."
-      ],
-      typeSpeed: 7,
-      contentType: 'html'
-    });
-  }, 100);
