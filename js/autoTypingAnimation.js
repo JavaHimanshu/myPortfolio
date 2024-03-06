ed3f86a5861c05c8828f5e5222fbea0b16517360
JavaHimanshu <140224061+JavaHimanshu@users.noreply.github.com>
2024-03-06 22:53:23 +0530

    Update autoTypingAnimation.js

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,7 +1,7 @@
 setTimeout(function() {
     $('.about-info-inner').typed({
       strings: [
-        "<span>Hi, I am Himanshu Chaurasiya</span> Completed Graduation in BSc.IT with skills like Java , Python , MYSQL , C# , MongoDB , C++ and Data Structure. An adaptable and responsible Student seeking an entry into IT Sector. An excellent academic record with active participation in activities shows myself to be selfmotivated, organized and capable of working under pressure. Science studies gave me a logical mind and problem solving attitude. Eagerly looking forward to learn and grow."
+        "<span>Hi, I am Himanshu Chaurasiya</span> Completed Graduation in BSc.IT with skills like Java , Python , MYSQL , C# , MongoDB , C++, Data Structure and Game Development. An adaptable and responsible Student seeking an entry into IT Sector. An excellent academic record with active participation in activities shows myself to be selfmotivated, organized and capable of working under pressure. Science studies gave me a logical mind and problem solving attitude. Eagerly looking forward to learn and grow."
       ],
       typeSpeed: 7,
       contentType: 'html'
